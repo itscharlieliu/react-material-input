efc10b5ae222ecce5eaed68d27c76fd3cea717ae
Charlie <itscharlieliu@gmail.com>
2020-12-20 10:43:32 -0800

    Refactor to use babel

--- dist/Input.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    helperText?: string;
-    label?: string;
-    error?: boolean;
-}
-export declare const Input: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HTMLInputElement>>;
